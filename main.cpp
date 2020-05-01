047911d7bf69427ae45b6d685eb92b460182bcc8
Mathias <mathias.sericola@gmail.com>
2020-05-01 16:23:11 +0200

    merge dev to romain

--- main.cpp
+++ main.cpp
@@ -88,7 +88,8 @@ void menu()
         case 5 :
         {
             Svgfile svgout;
-            nouv.centralite_intermediarite();
+            //nouv.centralite_intermediarite();
+            nouv.intermediarite();
             nouv.afficher_Svgfile(svgout);
             break;
         }
